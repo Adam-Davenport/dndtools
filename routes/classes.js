e04f68649daa4c34dc34abafceca969427ec9d54
Adam Davenport <adavenpo@purdue.edu>
2017-02-15 11:50:12 -0500

    Adding in index and new page. Will be storing classes in database and generating pages from templates

--- routes/classes.js
+++ routes/classes.js
@@ -1,8 +1,23 @@
 var express = require('express'),
 	router  = express.Router()
 
+// Main page with links to all of the class pages
+router.get('/', function (req, res) {
+	res.render('classes/index', {title: 'Classes'})
+})
+
+// New Page
+router.get('/new', function (req, res) {
+	res.render('classes/new', {title: 'Add A New Class'})
+})
+
 // Artificer
 router.get('/artificer', function (req, res) {
+	res.render('classes/artificer', {title: 'Artificer'})
+})
+
+// Cleric
+router.get('/cleric', function (req, res) {
 	res.render('classes/artificer', {title: 'Artificer'})
 })
 
